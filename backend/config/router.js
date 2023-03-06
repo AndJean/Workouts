24c388993e5eb2a5958ef658816ca0e2a1c3c409
AndJean <jean.cpregnon@gmail.com>
2023-03-06 19:17:24 +0000

    Add CRUD method for the workout path

--- backend/config/router.js
+++ backend/config/router.js
@@ -5,5 +5,14 @@ const router = express.Router();
 router.get('/workouts', (req, res) => {
     res.status(200).json('All workouts!');
 });
+router.post('/workouts', (req, res)=>{
+    res.status(200).json('New workout added!');
+});
+router.put('/workouts', (req, res)=>{
+    res.status(200).json('Workout updated!');
+});
+router.delete('/workouts', (req, res)=>{
+    res.status(200).json('Workout deleted!');
+});
 
 export default router;
