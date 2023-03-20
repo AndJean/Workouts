96b3a64e58b4677d68bdd11589a00a9480f5a1c5
AndJean <jean.cpregnon@gmail.com>
2023-03-20 19:27:06 +0000

    Route Updated and Db created

--- backend/config/router.js
+++ backend/config/router.js
@@ -1,18 +1,32 @@
 import express from 'express';
+import workouts from '../db/workout.js';
+
 
 const router = express.Router();
 
 router.get('/workouts', (req, res) => {
-    res.status(200).json('All workouts!');
+    console.log(workouts.find(workout => workout._id === req.query._id))
+    res.status(200).json(workouts);
 });
-router.post('/workouts', (req, res)=>{
-    res.status(200).json('New workout added!');
+router.post('/workouts', (req, res) => {
+    const { body } = req;
+    console.log(body);
+    workouts.push(body);
+    res.status(200).json(workouts);
 });
-router.put('/workouts', (req, res)=>{
-    res.status(200).json('Workout updated!');
+router.put('/workouts', ({ body, query }, res) => {
+    console.log(body);
+    console.log(query);
+    let updated_workout = workouts.find(workout => workout._id === query._id);//find the item with the id
+    console.log(updated_workout);
+    updated_workout = body;//update the item with the new data
+
+    const updated_workouts = workouts.filter(workout => workout._id !== query._id);//filter out the old item
+    res.status(200).json([...updated_workouts, workouts]);//return the updated item and the whole list
 });
-router.delete('/workouts', (req, res)=>{
-    res.status(200).json('Workout deleted!');
+router.delete('/workouts', ({query}, res) => {
+    console.log({query});
+    res.status(200).json('Workout deleted succesfuly!');
 });
 
 export default router;
---
+++ backend/db/workout.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        _id: "1",
+        name: 'Push-Ups',
+        day: 'Monday',
+        sets:4,
+        reps:15
+    },
+    {
+        _id: "2",
+        name: 'Pull-Ups',
+        day: 'Tuesday',
+        sets:6,
+        reps:12
+    },
+    {
+        _id: "3",
+        name: 'Squats',
+        day: 'Wednesday',
+        sets:5,
+        reps:8
+    },
+    {
+        _id: "4",
+        name: 'Bench Press',
+        day: 'Thursday',
+        sets:7,
+        reps:14
+    },
+    {
+        _id: "5",
+        name: 'Deadlifts',
+        day: 'Friday',
+        sets:8,
+        reps:13
+    },
+    {
+        _id: "6",
+        name: 'Bicep Curls',
+        day: 'Saturday',
+        sets:9,
+        reps:16
+    },
+    {
+        _id: "7",
+        name: 'Tricep Extensions',
+        day: 'Sunday',
+        sets:9,
+        reps:17
+    }   
+]
